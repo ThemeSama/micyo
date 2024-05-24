379617a0f00a9eae0fdd0c090622f83a3aa4ca4b
themesama <themesama9@gmail.com>
2024-05-24 18:07:23 +0300

    React forms doc

--- packages/react-forms/src/datalist/Datalist.tsx
+++ packages/react-forms/src/datalist/Datalist.tsx
@@ -3,6 +3,7 @@ import { forwardRef, HTMLInputTypeAttribute } from 'react';
 import useFieldId from '../hooks/useFieldId';
 import Input from '../input/Input';
 import Option from '../option/Option';
+import { BaseField } from '../types';
 
 export interface IList {
   label?: string;
@@ -12,17 +13,15 @@ export interface IList {
   options?: IList[];
 }
 
-export interface IDatalist {
-  id?: string;
-  label?: string;
-  name: string;
-  className?: string;
-  colSpan?: number;
-  desc?: string;
+export interface IDatalist extends BaseField {
   list: IList[];
   type?: HTMLInputTypeAttribute;
 }
 
+/**
+ * Datalist component contains a set of options that represent the permissible or recommended options available to choose from within other controls.
+ * With the **colSpan** prop, you can adjust how many columns an field will occupy in responsive design. It automatically displays validation warnings related to the corresponding field.
+ */
 const Datalist = forwardRef<HTMLInputElement, IDatalist>(
   ({ id, label, type = 'text', name, className, colSpan, desc, list, ...props }, ref) => {
     const fieldId = useFieldId(id),
