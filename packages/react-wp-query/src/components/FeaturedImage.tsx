7bf87a76bad17e351a3235c785844e03061ccd3c
themesama <themesama9@gmail.com>
2025-05-06 16:24:58 +0300

    feat: Added FeaturedImage component props for img tag

--- packages/react-wp-query/src/components/FeaturedImage.tsx
+++ packages/react-wp-query/src/components/FeaturedImage.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { usePost } from '../hooks';
 
-export const FeaturedImage = ({ size = 'full', className = '' }) => {
+type FeaturedImageProps = {
+  size?: string;
+  className?: string;
+} & React.ImgHTMLAttributes<HTMLImageElement>;
+
+export const FeaturedImage = ({ size = 'full', className = '', ...props }: FeaturedImageProps) => {
   const { _embedded } = usePost(),
     image = _embedded && _embedded['wp:featuredmedia']?.find((t) => t.media_type === 'image');
 
@@ -10,6 +15,11 @@ export const FeaturedImage = ({ size = 'full', className = '' }) => {
   const imageURL = image?.media_details?.sizes[size]?.source_url;
 
   return image ? (
-    <img src={imageURL} alt={image.alt_text} className={`micyo-article-image ${className}`} />
+    <img
+      src={imageURL}
+      alt={image.alt_text}
+      className={`micyo-article-image ${className}`}
+      {...props}
+    />
   ) : null;
 };
